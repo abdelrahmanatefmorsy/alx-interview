fcaeaba13eb3bd850a10b3704e1b578ae2915714
abdelrahman atef morsy <abdelrahman.atef.morsy@gmail.com>
2024-10-02 14:34:23 +0300

    hello

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+def pascal_triangle(n):
+    lst = []
+    if(n==0):
+        return lst
+    lst.append([1])
+    for i in range(1,n):
+        tmp = []
+        for j in range(i+1):
+            sum = 0
+            if(j<len(lst[i-1])):
+                sum+=lst[i-1][j]
+            if(j-1>=0):
+                sum+=lst[i-1][j-1]
+            tmp.append(sum)
+        lst.append(tmp)
+    return lst
+lst = pascal_triangle(10)
+for i in lst:
+    print(i)
