ecea0c76ca303f10b57b3e6234285045bc615b78
abdelrahman atef morsy <abdelrahman.atef.morsy@gmail.com>
2024-10-02 14:47:20 +0300

    hello

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,25 +1,24 @@
-
 #!/usr/bin/python3
-"""
-Moudule
-"""
+""" Moudule """
+
+
 def pascal_triangle(n):
     """ Function """
     lst = []
 
-    if(n <= 0):
+    if (n <= 0):
         return lst
 
     lst.append([1])
 
-    for i in range(1,n):
+    for i in range(1, n):
         tmp = []
         for j in range(i + 1):
             sum = 0
-            if(j < len(lst[i - 1])):
-                sum+=lst[i-1][j]
-            if(j-1 >= 0):
-                sum+=lst[i - 1][j - 1]
+            if (j < len(lst[i - 1])):
+                sum += lst[i-1][j]
+            if (j - 1 >= 0):
+                sum += lst[i - 1][j - 1]
             tmp.append(sum)
         lst.append(tmp)
     return lst
