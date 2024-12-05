542b003d13890a6fcba1cb5c690b7b26634bbe97
abdelrahman atef morsy <abdelrahman.atef.morsy@gmail.com>
2024-12-05 15:03:53 +0200

    hello

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -3,19 +3,18 @@
 
 
 def island_perimeter(grid):
-    res = 0
-    h = 0
-    w = 0
+    x1 = 1000
+    y1 = 1000
+    x2 = 0
+    y2 = 0    
     for i in range(len(grid)):
         tmp2 = 0
         for j in range(len(grid[i])):
             if grid[i][j] == 1:
-                ind = i
-                tmp = 0
-                tmp2 += 1
-                while (ind < len(grid) and grid[ind][j] == 1):
-                    ind += 1
-                    tmp += 1
-                h = max(h, tmp)
-        w = max(w, tmp2)
+                x1 = min(x1,i)
+                y1 = min(y1,j)
+                x2 = max(x2,i)
+                y2 = max(y2,j)
+    h = x2 - x1 + 1
+    w = y2 - y1  + 1
     return ((w + h) * 2)
