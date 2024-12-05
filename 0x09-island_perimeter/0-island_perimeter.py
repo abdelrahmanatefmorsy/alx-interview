b65478a1e126c0e188b2156998f5a79d34113c41
abdelrahman atef morsy <abdelrahman.atef.morsy@gmail.com>
2024-12-05 15:06:26 +0200

    hello

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -15,6 +15,7 @@ def island_perimeter(grid):
                 y1 = min(y1,j)
                 x2 = max(x2,i)
                 y2 = max(y2,j)
+                tmp = 1
     h = x2 - x1 + 1
     w = y2 - y1  + 1
-    return ((w + h) * 2)
+    return (((w + h) * 2) * tmp)
